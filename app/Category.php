553e7bb38017e03774e95be28771935f0005c6dc
Michal K <michal10203040@gmail.com>
2019-03-01 19:59:40 +0100

    Added relations
    Relations:
    User 1:n Product
    Product 1:n Images
    Product n:n Category

--- app/Category.php
+++ app/Category.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+use App\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function products() {
+        return $this->belongsToMany(Product::class);
+    }
 }
--- app/Image.php
+++ app/Image.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+use App\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
 }
