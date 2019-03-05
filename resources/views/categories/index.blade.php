fb32f4af737fdedca47d0b8fc2e3128040829574
Anthon Eldervik <Anthon@Eldervik.se>
2019-03-05 15:34:09 +0100

    Fixat 3 nivåer på nav

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -13,11 +13,26 @@
                             </button>
                         </h2>
                     </div>
-                    <div id="{{ $category->name }}" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
-                        <div class="card-body">
-                            @foreach($category->products as $product)
-                                <a class="nav-link my-1" href="/products/{{ $product->id }}">-{{ $product->name }}</a>
-                            @endforeach
+                    <div class="accordion" id="accordionExampleone">
+                        <div id="{{ $category->name }}" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                                @foreach($category->products as $product)
+                                <div class="card-header" id="headingOne">
+                                    <h2 class="mb-0">
+                                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#{{ $product->name }}" aria-expanded="true" aria-controls="collapseOne">
+                                                -{{ $product->name }}
+                                        </button>
+                                    </h2>
+                                </div>
+                                @endforeach
+                                <div id="{{ $product->name }}" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExampleone">
+                                    <div class="card-body">
+                                    @foreach($category->products as $product)
+                                        <a class="nav-link my-1" href="/products/{{ $product->id }}">{{ $product->id }} {{ $product->name }}</a>
+                                    @endforeach
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -29,13 +44,10 @@ h1{
     margin-left:10px;
 }
 .navwidth{
-    width:20%;
+    width:20%;  
     margin-left:10px;
 }
-.card-header{
-    padding:0px!important;
-}
-.card-body{
+.card-header, .card-body{
     padding:0px!important;
 }
 </style>
