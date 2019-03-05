3a9393afea9f3efface75792cbc11acfc45f9ad0
Anthon Eldervik <Anthon@Eldervik.se>
2019-03-05 15:10:40 +0100

    fixat sidebar

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,18 +1,41 @@
 @extends('layouts/app')
 
 @section('content')
-	<div class="container mt-3">
-		<h1>All Products</h1>
-
-		<ol>
-		@foreach($categories as $category)
-			<li><a href="/categories/{{ $category->id }}">{{ $category->name }}</a></li>
-			<ul>
-			@foreach($category->products as $product)
-				<li><a href="/products/{{ $product->id }}">{{ $product->name }}</a></li>
-			@endforeach
-			</ul>
-		@endforeach
-		</ol>
-	</div>
+<h1>All Categories</h1>
+	<div class="navwidth">
+            <div class="accordion" id="accordionExample">
+                @foreach($categories as $category)
+                <div class="card">
+                    <div class="card-header" id="headingOne">
+                        <h2 class="mb-0">
+                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#{{ $category->name }}" aria-expanded="true" aria-controls="collapseOne">
+                                    {{ $category->name }}
+                            </button>
+                        </h2>
+                    </div>
+                    <div id="{{ $category->name }}" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                        <div class="card-body">
+                            @foreach($category->products as $product)
+                                <a class="nav-link my-1" href="/products/{{ $product->id }}">-{{ $product->name }}</a>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
 @endsection
+<style>
+h1{
+    margin-left:10px;
+}
+.navwidth{
+    width:20%;
+    margin-left:10px;
+}
+.card-header{
+    padding:0px!important;
+}
+.card-body{
+    padding:0px!important;
+}
+</style>
